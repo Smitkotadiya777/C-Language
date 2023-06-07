c28c1a8787b1d0df1f4b94cb1a59fa2bcd5dc639
Smitkotadiya777 <135799039+Smitkotadiya777@users.noreply.github.com>
2023-06-07 10:37:31 +0530

    Add files via upload

---
+++ Itroduction/introduction.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+	
+	printf("Hello sir, My name is Smit.Right now i am studying in IT sector specialy Full-stacks.I am 21 years old.I am gradute in PHYSICAL EDUCATION");
+	
+	
+	
+	return 0;
+}
