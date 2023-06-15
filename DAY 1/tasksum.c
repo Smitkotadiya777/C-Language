c6c2050329030d591cc1c240606d63e51ac0123a
Smitkotadiya777 <135799039+Smitkotadiya777@users.noreply.github.com>
2023-06-15 13:23:06 +0530

    Add files via upload

---
+++ DAY 1/tasksum.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+int main(){
+	
+	//sum//
+	
+	int a=10;
+	int b=10;
+	
+	int sum=a+b;
+	
+	printf("%d\n",sum);
+	
+	//subtraction//
+	
+	int c=20;
+	int d=10;
+	
+	int sub=c-d;
+	
+	printf("%d\n",sub);
+	
+	
+	//multiplication//
+	
+	int e=10;
+	int f=10;
+	
+	int multi=e*f;
+	
+	printf("%d\n",multi);
+	
+	//division//
+	
+	int s=100;
+	int m=10;
+	
+	int divi=s/m;
+	
+	printf("%d\n",divi);
+	
+	
+	//percent//
+	
+	int x=100;
+	int y=10;
+	
+	int percent=x%y;
+	
+	printf("%d",percent);
+	
+	
+	
+	return 0;
+}
