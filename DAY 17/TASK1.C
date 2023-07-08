5bf84b05bf920a4bb22ea34e682112aad7073e4f
Smitkotadiya777 <135799039+Smitkotadiya777@users.noreply.github.com>
2023-07-08 21:14:04 +0530

    Add files via upload

---
+++ DAY 17/TASK1.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+	int main(){
+		
+		int m, n;
+		
+		printf("Enter the size of row & col: ");
+		scanf("%d %d", &m, &n);
+		
+		int arr[m][n];
+		int i, j;
+		
+		for(i=0; i<m; i++){
+			
+			for(j=0; j<n; j++){
+				
+				printf("Enter the value of [%d] [%d]: ", i, j);
+				scanf("%d", &arr[i][j]);
+			}
+			printf("\n");
+		}
+		for(i=0; i<m; i++){
+			
+			for(j=0; j<n; j++){
+				
+				printf("%d ", arr[i][j]);
+				
+			}
+			printf("\n");
+		}		
+	
+		return 0;
+	}
